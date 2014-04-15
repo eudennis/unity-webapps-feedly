041fbb3bfb24f39c0af4513d37b34078769b10ac
Dennis Altermann <dennisaltermann@gmail.com>
2014-04-15 20:19:35 -0300

    Updates on code to fix.

--- Feedly.user.js
+++ Feedly.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
-// @include        https://*.feedly.com/*
-// @include        http://*.feedly.com/*
+// @include https://*.feedly.com/*
+// @include https://www.feedly.com/*
+// @include https://*.feedly.com/
+// @include http://www.feedly.com/*
 // @require        utils.js
 // ==/UserScript==
 
-window.Unity = external.getUnityObject(1.0);
+window.Unity = external.getUnityObject(1);
 
 function unityLoaded() {
     var uri = window.location.toString();
