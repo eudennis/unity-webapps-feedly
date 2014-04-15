4afcffb5e2f2e142d3ab8f49fe5bd1b184548e98
Dennis Altermann <dennisaltermann@gmail.com>
2014-04-15 20:41:18 -0300

    / ‘Feedly.user.js’

--- Feedly.user.js
+++ Feedly.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @include https://*.feedly.com/*
-// @include https://www.feedly.com/*
-// @include https://*.feedly.com/
 // @include http://www.feedly.com/*
+// @include https://www.feedly.com/*
+// @include http://*.feedly.com/*
+// @include https://*.feedly.com/*
 // @require        utils.js
 // ==/UserScript==
 
-window.Unity = external.getUnityObject(1);
+window.Unity = external.getUnityObject(1.0);
 
 function unityLoaded() {
     var uri = window.location.toString();
